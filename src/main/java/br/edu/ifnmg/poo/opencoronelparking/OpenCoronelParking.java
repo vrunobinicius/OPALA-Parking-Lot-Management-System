e66f028559f20c1f841adf94240087938ba0513d
vrunobinicius <bvan@aluno.ifnmg.edu.br>
2023-11-26 04:15:45 -0300

    Login Screen Development

---
+++ src/main/java/br/edu/ifnmg/poo/opencoronelparking/OpenCoronelParking.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifnmg.poo.opencoronelparking;
+
+/**
+ *
+ * @author bvan &lt;Bruno Vinícius at ifnmg&gt;
+ */
+public class OpenCoronelParking {
+
+    public static void main(String[] args) {
+        LoginScreen.main(null);
+    }
+}
