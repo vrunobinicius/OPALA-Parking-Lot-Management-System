bbc7f32b04faa7506e59b2e2421a9fb44c98a82f
vrunobinicius <bvan@aluno.ifnmg.edu.br>
2023-11-26 13:49:50 -0300

    Alteração do Nome do Projeto, Nome de Pacote e Importações

--- src/main/java/br/edu/ifnmg/poo/opala/LoginScreen.java
+++ src/main/java/br/edu/ifnmg/poo/opala/LoginScreen.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package br.edu.ifnmg.poo.opencoronelparking;
+package br.edu.ifnmg.poo.opala;
 
 /**
  *
--- src/main/java/br/edu/ifnmg/poo/opala/Opala.java
+++ src/main/java/br/edu/ifnmg/poo/opala/Opala.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package br.edu.ifnmg.poo.opencoronelparking;
+package br.edu.ifnmg.poo.opala;
 
 /**
  *
